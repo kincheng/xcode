2d186ddca01b853d3b2edf5aaabcabd84d36aa65
Kin Cheng <kincheng@Kins-MacBook-Air.local>
2019-01-25 21:20:50 -0500

    first commit of xcode excerise

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Kin Cheng on 8/19/18.
+//  Copyright © 2018 Kin Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
